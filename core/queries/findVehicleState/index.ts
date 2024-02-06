66e67b912d8798109b83ddfc69843f450f7c6598
Michal Maszkiewicz <mmaszkiewicz@fastmail.co.uk>
2024-02-06 10:29:41 +0000

    helpful comments

--- core/queries/findVehicleState/index.ts
+++ core/queries/findVehicleState/index.ts
@@ -7,6 +7,8 @@ const cache = new NodeCache({ stdTTL: parseInt(getEnv('CACHE_TTL')) });
 
 export async function findVehicleState(queryInput: FindVehicleStateQuery) {
   const cacheKey = `${queryInput.vehicleId}/${queryInput.timestamp}`;
+  // TODO Ideally we would a cache service (Amazon ElastiCache) shared between multiple instances of this service
+  // But in order to make this run for you locally I've used local cache instead
   const cacheFound = cache.get(cacheKey);
   if (cacheFound) {
     return cacheFound;
